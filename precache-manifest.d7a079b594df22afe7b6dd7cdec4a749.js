5f322a45159462fed5a8551469fcbde4df07a536
geetha vani <geetha53813@gmail.com>
2020-05-21 18:56:08 +0530

    Updates

---
+++ precache-manifest.d7a079b594df22afe7b6dd7cdec4a749.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec41c574ab3643c936d764d6a0685807",
+    "url": "/heros/index.html"
+  },
+  {
+    "revision": "799e1783a5f2fe2e1d80",
+    "url": "/heros/static/css/main.8acd49a5.chunk.css"
+  },
+  {
+    "revision": "baf60169fad2374722d4",
+    "url": "/heros/static/js/2.b055ce50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/heros/static/js/2.b055ce50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "799e1783a5f2fe2e1d80",
+    "url": "/heros/static/js/main.27cf90f3.chunk.js"
+  },
+  {
+    "revision": "2ec3a4f267aa3f97add2",
+    "url": "/heros/static/js/runtime-main.9666d4e1.js"
+  }
+]);
